1d26af55b8849b122b1f616606bbf9519b1c1c29
Ian_Li <syukranabdullah@madrasah.kemenag.go.id>
2026-06-24 15:22:14 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,171 @@
-# M-ship
+# M-Ship
+
 M-Ship is a PyTorch project for SAR ship image background generation and inpainting. It uses a diffusion model to preserve ship regions while regenerating masked background regions under different scene conditions such as offshore and inshore.
+
+
+## Source Acknowledgements
+
+This project reuses and adapts code from the following open-source projects. Add or update exact links here before release:
+
+- Ship-Go / SAR image generation reference: [Ship-Go](https://github.com/XinZhangRadar/Ship-Go/blob/main/README.md)
+- SSDD dataset reference: [Official-SSDD](https://github.com/TianwenZhang0825/Official-SSDD)
+- RF-DETR downstream detector reference: [roboflow/rf-detr](https://github.com/roboflow/rf-detr)
+
+## Weights
+
+
+- Pretrained M-Ship weights:[Baidu Netdisk](https://pan.baidu.com/s/1Afh2asn-LiBgIIaPjqi9pQ)  #key==kkyy 
+- Generated SAR images: TODO: add generated image archive or gallery link
+- Downstream detector weights:[RF-DETR fine-tuned weight link](https://pan.baidu.com/s/1Afh2asn-LiBgIIaPjqi9pQ)#key==kkyy 
+
+
+
+## Project Structure
+
+```text
+M-ship/
+  config/
+    sard.json                     # main train/test configuration
+  core/
+    base_model.py                 # training/test base class
+    base_network.py               # network init utilities
+    logger.py                     # log and image writer
+    praser.py                     # config parser and experiment folder setup
+    util.py                       # seed/device/helpers
+  data/
+    dataset.py                    # SSDD/SAR dataset, masks, env labels
+    util/mask.py                  # mask helpers
+  models/
+    model.py                      # SARD trainer/test wrapper
+    network.py                    # BCDif diffusion network
+    loss.py                       # loss functions
+    metric.py                     # MAE, IS, etc.
+    guided_diffusion_modules/     # guided-diffusion style UNet
+    sr3_modules/                  # SR3 style UNet
+  preprocess/
+    mirflickr25k_preprocess.py
+  slurm/
+    ssdd.slurm                    # cluster job example
+  eval.py                         # FID / Inception Score evaluation
+  run.py                          # train/test entry point
+  requirements.txt
+```
+
+## Environment
+
+Install dependencies in a Python environment with PyTorch and CUDA support:
+
+```bash
+pip install -r requirements.txt
+```
+
+Main dependencies:
+
+- `torch`
+- `torchvision`
+- `numpy`
+- `opencv-python`
+- `tqdm`
+- `tensorboardX`
+- `clean-fid`
+
+The default config uses CUDA. Set `gpu_ids` in `config/sard.json` or pass `-gpu` at runtime.
+
+## Data Format
+
+The dataset class is `I2IDataset` in `data/dataset.py`.
+
+Expected data layout:
+
+```text
+datasets/sargen/
+  flist/
+    train.flist
+    valid.flist
+    test.flist
+    train_offshore.flist
+    train_inshore.flist
+    test_offshore.flist
+    test_inshore.flist
+    offshore.txt
+    inshore.txt
+  image/
+    000001.jpg
+    ...
+  Annotations_seg/
+    000001.xml
+    ...
+```
+
+`flist` files contain image paths or filenames used by the dataset loader. Environment labels are inferred by checking whether an image filename is listed in `offshore.txt` or `inshore.txt`.
+
+
+
+
+
+## Configuration
+
+The main configuration is [config/sard.json](config/sard.json).
+
+
+
+
+## Training
+
+Run training with:
+
+```bash
+python run.py -p train -c config/sard.json
+```
+
+
+The parser creates a timestamped experiment folder:
+
+```text
+experiments/train_i2i_sar_<timestamp>/
+  code/             # snapshot of config/core/data/models/slurm and scripts
+  tb_logger/        # TensorBoard logs
+  results/          # validation images
+  checkpoint/       # saved checkpoints
+  config.json       # resolved runtime config
+  train.log
+```
+
+## Testing / Generation
+
+Run generation with:
+
+```bash
+python run.py -p test -c config/sard.json
+```
+
+Outputs are written under:
+
+```text
+experiments/test_i2i_sar_<timestamp>/results/
+```
+
+The test loop saves:
+
+- `GT_*`: original image
+- `Process_*`: intermediate diffusion samples
+- `Out_*`: final generated output
+- `Mask_*`: mask visualization
+
+
+## RF-DETR Detection Usage
+
+
+
+
+## Citation
+
+
+```bibtex
+@misc{mship,
+  title = {M-Ship:employing multi-condition diffusion model to generate a SAR image ship target detection dataset},
+  author = {Ian_li,WANG_xinyan},
+  year = {2026},
+  url = {https://github.com/Ian-Li09/M_ship}
+}
+```
---
+++ data/dataset.py
@@ -0,0 +1,349 @@
+import torch.utils.data as data
+from sympy import is_amicable
+from torchvision import transforms
+from PIL import Image
+import os
+import torch
+import numpy as np
+
+from util.mask import (bbox2mask, bboxes2mask, brush_stroke_mask, get_irregular_mask, random_bbox, random_cropping_bbox)
+import xml.etree.ElementTree as ET
+import xml.dom.minidom
+import cv2
+
+from pycocotools.coco import COCO
+import  os, cv2, shutil
+from lxml import etree, objectify
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
+import time
+import json
+
+IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+]
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+def make_dataset(dir):
+    # import pdb;pdb.set_trace()
+    if os.path.isfile(dir):
+        images = [i for i in np.genfromtxt(dir, dtype=str, encoding='utf-8')]
+    else:
+        images = []
+        assert os.path.isdir(dir), '%s is not a valid directory' % dir
+        for root, _, fnames in sorted(os.walk(dir)):
+            for fname in sorted(fnames):
+                if is_image_file(fname):
+                    path = os.path.join(root, fname)
+                    images.append(path)
+
+    return images
+
+def pil_loader(path):
+    return Image.open(path).convert('RGB')
+
+
+# For SSDD
+class I2IDataset(data.Dataset):
+    CLASSES = ('ship',)
+    ENVS = ('offshore','inshore')
+    def __init__(self, data_root, mask_config={}, data_len=-1, image_size=[256, 256], loader=pil_loader,output_anns=None, out_anns_path=None):
+        imgs = make_dataset(data_root)
+        self.ENV_image_list = {env: make_dataset(os.path.join(data_root.split('flist')[0], 'flist',env+'.txt')) for env in self.ENVS}
+        if data_len > 0:
+            self.imgs = imgs[:int(data_len)]
+        else:
+            self.imgs = imgs
+        self.tfs = transforms.Compose([
+                transforms.Resize((image_size[0], image_size[1])),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
+        ])
+        self.loader = loader
+        self.mask_config = mask_config
+        self.mask_mode = self.mask_config['mask_mode']
+        self.image_size = image_size
+        self.env2label = {env: i for i, env in enumerate(self.ENVS)}
+        self.cat2label = {cat: i for i, cat in enumerate(self.CLASSES)}
+        # self.output_anns = output_anns
+        # self.out_anns_path = out_anns_path
+
+    def __getitem__(self, index):
+        ret = {}
+        ann = {}
+        path = self.imgs[index]
+        img = self.loader(path)
+        img_size = np.array(img.size) #(w,h)
+        img = self.tfs(img)
+        img_tf_size = img.shape[1:] #(h,w)
+        w_scale = img_tf_size[1]/img_size[0]
+        h_scale = img_tf_size[0]/img_size[1]
+        ann['height'] = img_tf_size[0]
+        ann['weight'] = img_tf_size[1]
+        ann['depth'] = 1 # for SAR images
+        ann['filename'] ='Out_'+path.split('/')[-1]
+        if self.mask_mode == 'det':
+            scale_factor = np.array([w_scale, h_scale, w_scale, h_scale],dtype=np.float32)
+            ann_path = path.replace('image','Annotations').replace('jpg','xml')
+            # ann = self.get_ann_info(ann_path)
+            # bboxes = self.resize_bboxes(ann['bboxes'],scale_factor)
+            # mask = self.get_mask(bboxes)
+            mask, objects = self.get_mask_from_det(ann_path, scale_factor)
+            if self.output_anns:
+                ann['objects'] = objects
+                self.genxml(self.out_anns_path, ann)
+        elif self.mask_mode == 'seg':
+            scale_factor = np.array([w_scale, h_scale],dtype=np.float32)
+            ann_path = path.replace('image','Annotations_seg').replace('jpg','xml')
+            mask = self.get_mask_from_seg(ann_path, scale_factor)
+
+        # Keep instances, background plus noise
+        cond_image = img*(1. - mask) + mask*torch.randn_like(img)
+        #
+        mask_img = img*(1. - mask) + mask
+
+        #ENVS:
+        for env in self.ENV_image_list.keys():
+            if path.split('/')[-1] in self.ENV_image_list[env]:
+                image_env = self.env2label[env]
+                break
+        # mask_img = (mask_img + 1)*127
+        # cv2.imwrite('mask_img.jpg',mask_img.permute(1,2,0).numpy())
+        # image_env = 1
+        ret['gt_image'] = img[0].unsqueeze(0)
+        ret['cond_image'] = cond_image[0].unsqueeze(0)
+        ret['mask_image'] = mask_img[0].unsqueeze(0)
+        ret['mask'] = mask[0].unsqueeze(0)
+        ret['path'] = path.rsplit("/")[-1].rsplit("\\")[-1]
+        ret['image_env'] = image_env
+        return ret
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def resize_det_anns(self,xml_path,scale_factor):
+        objects = self.get_ann_info(xml_path)
+        bboxes = self.resize_bboxes(objects['bboxes'],scale_factor)
+        objects['bboxes'] = bboxes
+        return objects
+
+
+    # def get_mask_from_det(self, xml_path, scale_factor):
+    #     objects = self.resize_det_anns(xml_path, scale_factor)
+    #     #import pdb;pdb.set_trace()
+    #     ann = self.get_ann_info(xml_path)
+    #     bboxes = self.resize_bboxes(ann['bboxes'],scale_factor)
+    #     mask = bboxes2mak(self.image_size, objects['bboxes'])
+    #     return torch.from_numpy(mask).permute(2,0,1), objects
+
+
+    # def get_mask_from_seg(self, xml_path, scale_factor):
+
+    #     DomTree = xml.dom.minidom.parse(xml_path)
+    #     annotation = DomTree.documentElement
+    #     filenamelist = annotation.getElementsByTagName('filename')
+    #     filename = filenamelist[0].childNodes[0].data
+    #     objectlist = annotation.getElementsByTagName('object')
+    #     height, width = self.image_size[:2]
+    #     mask = np.ones((height, width, 1), dtype=np.uint8)
+    #     for objects in objectlist:
+    #         namelist = objects.getElementsByTagName('name')
+    #         objectname = namelist[0].childNodes[0].data
+    #         bndbox_list = objects.getElementsByTagName('bndbox')
+    #         one_segm_points_list = []
+    #         for bndbox in bndbox_list:
+    #             xmin = int(float(bndbox.getElementsByTagName('xmin')[0].childNodes[0].data))
+    #             ymin = int(float(bndbox.getElementsByTagName('ymin')[0].childNodes[0].data))
+    #             xmax = int(float(bndbox.getElementsByTagName('xmax')[0].childNodes[0].data))
+    #             ymax = int(float(bndbox.getElementsByTagName('ymax')[0].childNodes[0].data))
+    #             one_segm_points_list = [
+    #                 [xmin, ymin],
+    #                 [xmax, ymin],
+    #                 [xmax, ymax],
+    #                 [xmin, ymax],
+    #             ]
+
+    #         pts = np.array(one_segm_points_list, np.int32)
+    #         pts = pts * scale_factor
+    #         pts = np.fix(pts).astype(int)
+
+    #         cv2.fillPoly(mask, [pts], 0)
+
+    #     return torch.from_numpy(mask).permute(2,0,1)
+    def get_mask_from_seg(self, xml_path, scale_factor):
+
+        DomTree = xml.dom.minidom.parse(xml_path)
+        annotation = DomTree.documentElement
+        filenamelist = annotation.getElementsByTagName('filename')
+        filename = filenamelist[0].childNodes[0].data
+        objectlist = annotation.getElementsByTagName('object')
+        height, width = self.image_size[:2]
+        mask = np.ones((height, width, 1), dtype=np.uint8)
+        for objects in objectlist:
+            namelist = objects.getElementsByTagName('name')
+            objectname = namelist[0].childNodes[0].data
+            segm = objects.getElementsByTagName('segm')
+            one_segm_points_list = []
+            for one_segm in segm:
+                points = one_segm.getElementsByTagName('point')
+                for point in points:
+                    x = point.childNodes[0].data.split(',')[0]
+                    y = point.childNodes[0].data.split(',')[1]
+
+                    one_segm_points_list.append([x, y])
+
+            pts = np.array(one_segm_points_list, np.int32)
+            pts = pts * scale_factor
+            pts = np.fix(pts).astype(int)
+
+            cv2.fillPoly(mask, [pts], 0)
+        # get the mask; the region of ship fill 0&the region of the bg fill 1
+        return torch.from_numpy(mask).permute(2, 0, 1)
+
+
+    def resize_bboxes(self, bboxes, scale_factor):
+        """Resize bounding boxes with ``results['scale_factor']``."""
+        bboxes = bboxes * scale_factor
+        return np.fix(bboxes).astype(np.int)
+    
+
+
+
+    def get_ann_info(self, xml_path):
+        """Get annotation from XML file by index.
+
+        Args:
+            idx (int): Index of data.
+
+        Returns:
+            dict: Annotation info of specified index.
+        """
+        tree = ET.parse(xml_path)
+        root = tree.getroot()
+        bboxes = []
+        labels = []
+        bboxes_ignore = []
+        labels_ignore = []
+        for obj in root.findall('object'):
+            name = obj.find('name').text
+            if name not in self.CLASSES:
+                continue
+            label = self.cat2label[name]
+            bnd_box = obj.find('bndbox')
+            # TODO: check whether it is necessary to use int
+            # Coordinates may be float type
+            bbox = [
+                int(float(bnd_box.find('xmin').text)),
+                int(float(bnd_box.find('ymin').text)),
+                int(float(bnd_box.find('xmax').text)),
+                int(float(bnd_box.find('ymax').text))
+            ]
+            ignore = False
+ 
+            bboxes.append(bbox)
+            labels.append(label)
+        if not bboxes:
+            bboxes = np.zeros((0, 4))
+            labels = np.zeros((0, ))
+        else:
+            bboxes = np.array(bboxes, ndmin=2) - 1
+            labels = np.array(labels)
+
+        ann = dict(
+            bboxes=bboxes.astype(np.float32),
+            labels=labels.astype(np.int64))
+        return ann
+
+    def genxml(self,ann_savepath='VOC2007/Annotations/',ann=None):
+        #import pdb;pdb.set_trace()
+        filename = ann['filename']
+        objs = []
+        bboxes = ann['objects']['bboxes']
+        labels = ann['objects']['labels']
+        for i, bbox in enumerate(bboxes):
+            name = self.CLASSES[labels[i]]
+
+            xmin = (int)(bbox[0])
+            ymin = (int)(bbox[1])
+            xmax = (int)(bbox[2])
+            ymax = (int)(bbox[3])
+            obj = [name, xmin, ymin, xmax, ymax]
+            if not(xmin-xmax==0 or ymin-ymax==0):
+                objs.append(obj)  
+        annopath = os.path.join(ann_savepath,filename[:-3] + "xml") 
+       
+
+        E = objectify.ElementMaker(annotate=False)
+        anno_tree = E.annotation(
+            E.folder('VOC'),
+            E.filename(filename),
+            E.source(
+                E.database('ShipGo'),
+                E.annotation('VOC'),
+                E.image('ShipGo')
+            ),
+            E.size(
+                E.width(ann['weight']),
+                E.height(ann['height']),
+                E.depth(ann['depth'])
+            ),
+            E.segmented(0)
+        )
+
+        for obj in objs:
+            E2 = objectify.ElementMaker(annotate=False)
+            anno_tree2 = E2.object(
+                E.name(obj[0]),
+                E.pose(),
+                E.truncated("0"),
+                E.difficult(0),
+                E.bndbox(
+                    E.xmin(obj[1]),
+                    E.ymin(obj[2]),
+                    E.xmax(obj[3]),
+                    E.ymax(obj[4])
+                )
+            )
+            anno_tree.append(anno_tree2)
+        etree.ElementTree(anno_tree).write(annopath, pretty_print=True)
+
+
+if __name__ == '__main__':
+    import os
+    data_root = '/Users/M-ship/datasets/sargen/flist/train.flist'
+    flist_dir = os.path.join(os.path.dirname(data_root.split('flist')[0]), 'flist')
+    env2files = {}
+    for env_name in I2IDataset.ENVS:
+        path = os.path.join(flist_dir, env_name + '.txt')
+        with open(path) as f:
+            env2files[env_name] = {line.strip().split('/')[-1] for line in f if line.strip()}
+
+    images = make_dataset(data_root)
+    print(f'数据集大小: {len(images)}')
+    from collections import Counter
+    env_counter = Counter()
+    for fullpath in images:
+        fname = fullpath.split('/')[-1]
+        env = None
+        for env_name, files in env2files.items():
+            if fname in files:
+                env = I2IDataset.ENVS.index(env_name)
+                break
+        if env is None:
+            env = -1
+        env_counter[env] += 1
+        label = I2IDataset.ENVS[env] if env >= 0 else 'unknown'
+        print(f'  {fname:20s}  image_env={env} ({label})')
+    print()
+    print(f'=== 分布 ===')
+    for env_name in I2IDataset.ENVS:
+        env = I2IDataset.ENVS.index(env_name)
+        print(f'  {env_name} ({env}): {env_counter.get(env, 0)} 张')
+    if -1 in env_counter:
+        print(f'  unknown  (-1): {env_counter[-1]} 张')
+#
+#
